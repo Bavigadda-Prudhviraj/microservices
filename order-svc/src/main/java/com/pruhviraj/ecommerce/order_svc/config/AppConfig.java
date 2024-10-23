1a3c1b75ab4b426b9e5ddad86b91f8e8c5e66939
Bavigadda Prudhviraj <prudhviraj729@gmail.com>
2024-10-23 15:55:22 +0530

    Zipkin still need to work

--- order-svc/src/main/java/com/pruhviraj/ecommerce/order_svc/config/AppConfig.java
+++ order-svc/src/main/java/com/pruhviraj/ecommerce/order_svc/config/AppConfig.java
@@ -1,8 +1,12 @@
 package com.pruhviraj.ecommerce.order_svc.config;
 
+import feign.Capability;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class AppConfig {
@@ -10,5 +14,11 @@ public class AppConfig {
     public ModelMapper modelMapper(){
         return new ModelMapper();
     }
+    @Bean
+    public Capability capability(final MeterRegistry registry) {
+        return new MicrometerCapability(registry);
+    }
+
+
 }
 
